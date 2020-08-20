7498ca3c0238d9f189e5863e57cb8b63a6697f14
Davit-000 <hovhannisyan006@gmail.com>
2020-08-20 17:00:39 +0400

    config file generator script

--- scripts/config.file.js
+++ scripts/config.file.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-// console.log(global.process);
-//
-console.log(path.resolve(__dirname, '../../../'));
+const projectPath = path.resolve(__dirname, '../../../');
 
-// console.log(path.dirname(require.main.filename));
+const code = `
+  export default {
+    origin: typeof window !== "undefined" ? window.location.origin : '',
+    backend: 'laravel'
+  }
+`;
 
+fs.access(projectPath, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+  if (err) {
+    console.error(`${projectPath} ${err.code === 'ENOENT' ? 'does not exist' : 'is read-only'}`);
+  } else {
+    fs.writeFileSync(`${projectPath}/restify.config.js`, code, 'utf8');
 
-// const filePath = '../';
-//
-// fs.access('restify.config.js', fs.constants.F_OK | fs.constants.W_OK, (err) => {
-//   if (err) {
-//     console.error(
-//       `${file} ${err.code === 'ENOENT' ? 'does not exist' : 'is read-only'}`);
-//   } else {
-//     console.log(`${file} exists, and it is writable`);
-//   }
-// });
+    console.log('restify.config.js file has been created successfully.');
+  }
+});
