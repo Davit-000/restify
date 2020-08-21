a0b1a804d0ad3dc550fb17f5ae0117d0cfe47035
Davit-000 <hovhannisyan006@gmail.com>
2020-08-21 18:05:56 +0400

    clean index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,24 +3,7 @@
 import { Model } from "./model";
 import { Config } from "./config";
 
-// declare module 'vue-restify' {
-//   import { Model } from "./model";
-//   import { Config } from "./config";
-//
-//   export = {
-//     Restify: Model,
-//     RestifyConfig: Config,
-//   };
-// }
-
-// declare const Restify: Model;
-// declare const RestifyConfig: Config;
-
 export {
   Model as Restify,
   Config as RestifyConfig
 }
-// export {
-//   Restify,
-//   RestifyConfig
-// }
