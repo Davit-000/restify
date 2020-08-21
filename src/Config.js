81cfb046ba4edcd5cb14e0fbc9bdd89e9692fd5c
Davit-000 <hovhannisyan006@gmail.com>
2020-08-21 11:56:24 +0400

    Restify global config coorection.

--- src/Config.js
+++ src/Config.js
@@ -1,17 +1,17 @@
 import { get, set } from "lodash";
-import config from "../restify.config";
+import { Model } from "./Model";
 
 
 export class Config {
   static all() {
-    return config;
+    return Model.$config;
   }
 
   static get(path, defaultValue) {
-    return get(config, path, defaultValue);
+    return get(Model.$config, path, defaultValue);
   }
 
   static set(path, value) {
-    set(config, path, value);
+    set(Model.$config, path, value);
   }
 }
